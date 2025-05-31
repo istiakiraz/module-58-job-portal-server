a33bbfb283ae3af8ee7a11d3d8f8a2342f8bb8d6
istiakiraz <istiakiraz@gmail.com>
2025-05-31 23:59:18 +0600

    added a mongo database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
 
 const app = express();
 
@@ -14,7 +16,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.un5m5dm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.un5m5dm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +31,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    //collection
+    const jobsCollection = client.db('jobsPortal').collection('jobs');
+
+        //jobs api 
+        app.get('/jobs', async(req, res)=>{
+            const cursor = jobsCollection.find();
+            const result = await cursor.toArray();
+
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
