d7716670812b016a01dcf9eafd4caea31d0ca9c8
istiakiraz <istiakiraz@gmail.com>
2025-06-02 00:09:46 +0600

    added a single id get method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
@@ -12,8 +12,6 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.un5m5dm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -42,7 +40,17 @@ async function run() {
             const result = await cursor.toArray();
 
             res.send(result)
+        });
+
+        app.get('/jobs/:id', async(req, res)=>{
+          const id = req.params.id;
+          const query = {_id: new ObjectId(id) }
+
+          const result = await jobsCollection.findOne(query);
+          res.send(result)
+
         })
+
 
 
 
