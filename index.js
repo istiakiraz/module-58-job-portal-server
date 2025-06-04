6e9bcfa6f41979901aa2aa33ebfe2ef6ea40aa44
istiakiraz <istiakiraz@gmail.com>
2025-06-05 05:41:25 +0600

    added a my posted job data

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
-    strict: true,
+    strict: false,
     deprecationErrors: true,
   }
 });
@@ -31,82 +31,112 @@ async function run() {
     await client.connect();
 
 
+
     //collection
     const jobsCollection = client.db('jobsPortal').collection('jobs');
     const applicationCollection = client.db('jobsPortal').collection('application')
 
-        //jobs api 
-        // app.get('/jobs', async(req, res)=>{
-        //     const cursor = jobsCollection.find();
-        //     const result = await cursor.toArray();
 
-        //     res.send(result)
-        // });
 
-        app.get('/jobs', async (req, res) => {
-    const search = req.query.search || '';
-    const category = req.query.category || '';
+    // jobs api 
+    // app.get('/jobs', async(req, res)=>{
+    //     const cursor = jobsCollection.find();
+    //     const result = await cursor.toArray();
 
-    const query = {};
+    //     res.send(result)
+    // });
 
-    
-    if (search) {
+
+    await jobsCollection.createIndex({ title: "text" });
+
+    app.get('/jobs', async (req, res) => {
+
+      const search = req.query.search || '';
+      const category = req.query.category || '';
+
+      const email = req.query.email
+      
+      const query = {};
+
+      if(email){
+        query.hr_email = email
+      }
+
+      if (search) {
         query.$text = { $search: search };
-    }
+      }
 
-    if (category) {
+      if (category) {
         query.category = category;
-    }
+      }
 
-        const result = await jobsCollection.find(query).toArray();
-        res.send(result);
-    
-});
-
-        app.get('/jobs/:id', async(req, res)=>{
-          const id = req.params.id;
-          const query = {_id: new ObjectId(id) }
-
-          const result = await jobsCollection.findOne(query);
-          res.send(result)
-
-        })
-
-        app.post('/jobs', async(req, res)=>{
-          const newJob = req.body;
-          console.log(newJob);
-          const result = await jobsCollection.insertOne(newJob);
-          res.send(result)
-
-
-        })
-
-        // job application related Api 
-
-        app.get('/applications', async(req, res)=>{
-          const email = req.query.email;
-
-          const query = {
-            applicant: email
-          }
-
-          const result = await applicationCollection.find(query).toArray()
-          res.send(result)
-
-        })
+      
 
 
 
+      const result = await jobsCollection.find(query).toArray();
+      res.send(result);
 
-        app.post('/applications', async(req, res)=>{
+    });
 
-          const application = req.body;
-          // console.log(application);
-          const result = await applicationCollection.insertOne(application);
+    // could be done but should not be done
+    // app.get('/jobByEmail', async (req, res) => {
+    //   const email = req.query.email;
 
-          res.send(result);
+    //   const query = { hr_email: email }
 
-        })
+    //   const result = await jobsCollection.find(query).toArray();
+    //   res.send(result)
+
+    // })
+
+
+    app.get('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+      const result = await jobsCollection.findOne(query);
+      res.send(result)
+
+    })
+
+    app.post('/jobs', async (req, res) => {
+      const newJob = req.body;
+      // console.log(newJob);
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result)
+
+
+    })
+
+
+
+    // job application related Api 
+
+    app.get('/applications', async (req, res) => {
+      const email = req.query.email;
+
+      const query = {
+        applicant: email
+      }
+
+      const result = await applicationCollection.find(query).toArray()
+      res.send(result)
+
+    })
+
+
+
+
+    app.post('/applications', async (req, res) => {
+
+      const application = req.body;
+      // console.log(application);
+      const result = await applicationCollection.insertOne(application);
+
+      res.send(result);
+
+    })
 
 
 
@@ -142,10 +172,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('jobs are ready')
+app.get('/', (req, res) => {
+  res.send('jobs are ready')
 })
 
-app.listen(port, ()=>{
-    console.log(`jobs portal sever on port ${port} `);
+app.listen(port, () => {
+  console.log(`jobs portal sever on port ${port} `);
 })
